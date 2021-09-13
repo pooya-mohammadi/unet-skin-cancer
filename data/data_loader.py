12ab2c8b9a2b4afd24a0a82bb88f213e45d1c45d
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-09-13 16:03:25 +0430

    Initial Commits

---
+++ data/data_loader.py
@@ -0,0 +1,10 @@
+from typing import Tuple
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+
+def get_loader(dataset_dir, ) -> Tuple[ImageDataGenerator, ImageDataGenerator, ImageDataGenerator]:
+    """This function returns 3 generators
+    train, val, test.
+
+    """
+    pass
