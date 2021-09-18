3a940f295c01be55ff7bd03c429b9f04f44ceb84
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-09-18 16:47:13 +0430

    AutoParser changed correctly

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,21 +1,45 @@
 from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
+from argparse import *
 
-def preprocessing(train_zip, test_zip, mask_train_zip, mask_test_zip,
-                 train_path, test_path, mask_train_path,mask_test_path):
-    with urlopen(train_zip) as zip_train:
+
+def preprocessing(args):
+    with urlopen(args[0]) as zip_train:
         with ZipFile(BytesIO(zip_train.read())) as zfile:
-            zfile.extractall(train_path)
-    with urlopen(test_zip) as zip_test:
+            zfile.extractall(args[4])
+    with urlopen(args[1]) as zip_test:
         with ZipFile(BytesIO(zip_test.read())) as zfile:
-            zfile.extractall(test_path)
-    with urlopen(mask_train_zip) as zip_mask_train:
+            zfile.extractall(args[5])
+    with urlopen(args[2]) as zip_mask_train:
         with ZipFile(BytesIO(zip_mask_train.read())) as zfile:
-            zfile.extractall(mask_train_path)
-    with urlopen(mask_test_zip) as zip_mask_test:
+            zfile.extractall(args[6])
+    with urlopen(args[3]) as zip_mask_test:
         with ZipFile(BytesIO(zip_mask_test.read())) as zfile:
-            zfile.extractall(mask_test_path)
+            zfile.extractall(args[7])
+    print("Preprocessing is done")
+if __name__ == "main":
+    parser = ArgumentParser()
+    print("salam")
+    # parser.add_argument("--url", default='mu url', help='this is the url')
+    train_zip_url = "https://isic-challenge-data.s3.amazonaws.com/2016/ISBI2016_ISIC_Part1_Training_Data.zip"
+    test_zip_url = "https://isic-challenge-data.s3.amazonaws.com/2016/ISBI2016_ISIC_Part1_Test_Data.zip"
+    mask_train_zip_url = "https://isic-challenge-data.s3.amazonaws.com/2016/ISBI2016_ISIC_Part1_Training_GroundTruth.zip"
+    mask_test_zip_url = "https://isic-challenge-data.s3.amazonaws.com/2016/ISBI2016_ISIC_Part1_Test_GroundTruth.zip"
+    train_path = "./train"
+    test_path = "/.test"
+    mask_train_path = "./masktrain"
+    mask_test_path = "./masktest"
+    default_list = [train_zip_url, test_zip_url, mask_train_zip_url, mask_test_zip_url,
+                        train_path, test_path, mask_train_path, mask_test_path]
+
+    parser = ArgumentParser()
+    parser.add_argument("--url", nargs="+", default=default_list)
+
+    args = parser.parse_args()
+    print(args.url)
+    preprocessing(args.url)
+
 
 #"https://isic-challenge-data.s3.amazonaws.com/2016/ISBI2016_ISIC_Part1_Training_Data.zip",
 #"https://isic-challenge-data.s3.amazonaws.com/2016/ISBI2016_ISIC_Part1_Training_GroundTruth.zip"
