eb2ad12dbe0a2c516da10540ab7b3c4b14e57687
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-09-21 15:47:11 +0430

    callbacks added to callback file and remove from train.py

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,4 +1,7 @@
-from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, LearningRateScheduler
+from params import get_args
+import sys
+
 
 
 def get_callbacks(model_path, early_stopping_p, save_weights_only=True, plateau_min_lr=0.0001, **kwargs):
@@ -17,4 +20,33 @@ def get_callbacks(model_path, early_stopping_p, save_weights_only=True, plateau_
                                   verbose=1
                                   )
     early_stopping = EarlyStopping(monitor="val_loss", patience=early_stopping_p, verbose=1)
-    return checkpoint, reduce_lr, early_stopping
+
+    #Unet callbacks
+    model_name = sys.argv[2]
+    args = get_args(model_name)
+    def get_lr_callback(batch_size=8, lr_start = args.lr_start, lr_min = args.lr_min):
+        lr_max     = 0.01 * batch_size
+        lr_ramp_ep = 5
+        lr_sus_ep  = 0
+        lr_decay   = 0.8
+   
+        def lrfn(epoch):
+            if epoch < lr_ramp_ep:
+                lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
+            
+            elif epoch < lr_ramp_ep + lr_sus_ep:
+                lr = lr_max
+            
+            else:
+                lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
+            
+            return lr
+
+        lr_callback = LearningRateScheduler(lrfn, verbose=False)
+        return lr_callback
+
+    estop_callback = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=5, mode='auto')
+    batch_size = 8
+    lr_callback = get_lr_callback(batch_size)  
+    return checkpoint, reduce_lr, early_stopping, estop_callback, lr_callback
+
