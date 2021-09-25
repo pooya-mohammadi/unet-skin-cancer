6d1e191347fa4709ca6212b93dafd73e897420bc
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-09-25 12:54:31 +0330

    [params] fix unet_conv_deconv args and also correct model name

--- params/__init__.py
+++ params/__init__.py
@@ -1,7 +1,9 @@
-from .unet import UnetRes50
-ARGUMENTS = dict(unet = UnetRes50
-                 # other args
-                 )
+from .unet_res50 import UnetRes50
+from . unet_conv_deconv import UnetConvDeconv
+ARGUMENTS = dict(unet_res50 = UnetRes50,
+                 unet_conv_deconv = UnetConvDeconv,
+              # other models
+              )
 
 
 def get_args(model_name):
