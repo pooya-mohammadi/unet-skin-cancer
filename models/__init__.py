6efb3c90bd19722535ad43ce9f1f0364173b1543
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-09-25 13:07:25 +0330

    [models][unet_conv_deconv] fixed class name as UnetConvDecon

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 from .unet_res50 import UnetRes50
-from .unet_conv_deconv import Unet_conv_deconv
+from .unet_conv_deconv import UnetConvDeconv
 
 MODELS = dict(unet_res50 = UnetRes50,
-              unet_conv_deconv = Unet_conv_deconv,
+              unet_conv_deconv = UnetConvDeconv,
               # other models
               )
 
