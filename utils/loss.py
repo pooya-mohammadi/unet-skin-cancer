1c3b2079245afe0be5353736395e9a18dc69efd9
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-09-25 11:08:48 +0330

    [utils][loss] each loss function seperated

--- utils/loss.py
+++ utils/loss.py
@@ -10,38 +10,41 @@ def dsc(y_true, y_pred):
     score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return score
 
-#Dice loss according dice coefficient
+
+# Dice loss according dice coefficient
 def dice_loss(y_true, y_pred):
     loss = 1 - dsc(y_true, y_pred)
     return loss
 
-#Jacard loss
-def jaccard_loss(output, target, axis=(0, 1, 2), smooth=1e-5): 
+
+# Jacard loss
+def jaccard_loss(output, target, axis=(0, 1, 2), smooth=1e-5):
     inse = tf.reduce_sum(output * target, axis=axis)
-    
+
     l = tf.reduce_sum(output * output, axis=axis)
     r = tf.reduce_sum(target * target, axis=axis)
-    
-    jaccard = 1- (inse + smooth) / (l + r - inse + smooth)
+
+    jaccard = 1 - (inse + smooth) / (l + r - inse + smooth)
     jaccard = tf.reduce_mean(jaccard)
 
     return jaccard
 
 
-#Tversky index
+# Tversky index
 def tversky(y_true, y_pred):
     y_true_pos = K.flatten(y_true)
     y_pred_pos = K.flatten(y_pred)
     true_pos = K.sum(y_true_pos * y_pred_pos)
-    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
-    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
+    false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
     alpha = 0.7
-    return (true_pos + 1)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + 1)
+    return (true_pos + 1) / (true_pos + alpha * false_neg + (1 - alpha) * false_pos + 1)
 
-#Focal loss according to tversky index
-def focal_tversky_loss(y_true,y_pred):
+
+# Focal loss according to tversky index
+def focal_tversky_loss(y_true, y_pred):
     pt_1 = tversky(y_true, y_pred)
     gamma = 0.75
-    return K.pow((1-pt_1), gamma)
+    return K.pow((1 - pt_1), gamma)
 
 
