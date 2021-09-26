e4b203345dae3b821782fbecb8dbb4ec3ff72e02
Pooya Mohammadi Kazaj <practical.ai.programming@gmail.com>
2021-09-26 10:53:53 +0330

    [streamlit] Fix weight Address

--- streamlit/config.py
+++ streamlit/config.py
@@ -1,13 +1,13 @@
 MODELS_TO_ADDR = {
-    "unet-res50": 'weights/unet_res50/weights',
-    "unet-conv-deconv" : 'weights/unet_conv_deconv/weights',
+    "unet_res50": 'weights/unet_res50/unet_model.h5',
+    "unet_conv_deconv": 'weights/unet_conv_deconv/weights',
 }
 
 MODELS_TO_ARGS = {
-    "unet-res50": {
+    "unet_res50": {
         'input_shape': (None, None, 3)
     },
-    "unet-conv-deconv": {
+    "unet_conv_deconv": {
         'input_shape': (None, None, 3)
     },
 }
