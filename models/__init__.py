2d8b2a8f1a783de5fe935cb7d3f508f465401c17
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-10-09 16:51:46 +0330

    [model][4 models files] unet attention files added

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,18 @@
 from .unet_res50 import UnetRes50
 from .unet_conv_deconv import UnetConvDeconv
-from .unet_attention import UnetAttention
+from .unet_attention_gate import GateUnet
 from .doubleunet import DoubleUnet
+from .unet_cbam import CbamUnet
+from .unet_cbam_gate import Cbam_Gate_Unet
+from  .unet_pyramid_cbam_gate import Pyramid_Cbam_Gate_Unet
 
 MODELS = dict(unet_res50=UnetRes50,
               unet_conv_deconv=UnetConvDeconv,
-              unet_attention=UnetAttention,
+              unet_attention_gate=GateUnet,
               double_unet=DoubleUnet,
+              unet_cbam = CbamUnet,
+              unet_cbam_gate = Cbam_Gate_Unet,
+              unet_pyramid_cbam_gate = Pyramid_Cbam_Gate_Unet,
               # other models
               )
 
