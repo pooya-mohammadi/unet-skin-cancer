960374ad523cab98b8b8f1eff11712a44dcaa534
Ali Shahedi <www.alishahedi3216@gmail.com>
2021-10-13 18:08:40 +0330

    [utils][squeezeexcitation] SE block fixed

--- utils/sqeezeexcitation.py
+++ utils/sqeezeexcitation.py
@@ -4,7 +4,19 @@ import tensorflow
 from tensorflow.keras.layers import *
 
 
-def squeezeexcite(x: tensorflow.keras.layers, ratio=16) -> tensorflow.keras.layers:
+def squeezeexcite(x: tensorflow.keras.layers, ratio: int = 16) -> tensorflow.keras.layers:
+    """
+    Explanations:
+    SE blocks are lightweight gating mechanism in the channel-wise relationships.
+    networks are able to learn now how to understand the importance of each feature
+    map in the stack of all the feature maps extracted after a convolution operation
+    and recalibrates that output to reflect that importance before passing the volume to the next layer
+    """
+
+    """
+    Code link :
+    https://github.com/rezazad68/BCDU-Net/blob/033a2e5768d87e04e39deacb4746ecf088518f63/Skin%20Lesion%20Segmentation/models.py#L187"""
+
     """
 
     Parameters
